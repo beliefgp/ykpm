f8948d33dceb266c4e862595a88cfff70078d7d7
beliefgp <ggjqzjgp103@qq.com>
2016-06-13 11:10:28 +0800

    增加help version命令

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ var fs = require('fs');
 var path = require('path');
 var optimist = require('optimist');
 
+// 帮助信息
+optimist.usage([
+	'Usage: ykpm [COMMAND] --config=[CONFIG_FILE]\n\n',
+	'ykpm build\n',
+	'ykpm [filename]\n',
+	'ykpm build [filename]\n',
+	'ykpm debug'
+].join(''));
+
 var argv = optimist.argv;
 
-//获取命令及参数
+// 帮助信息
+if (argv.help || argv.h) {
+	optimist.showHelp();
+	process.exit();
+}
+
+// 版本信息
+if (argv.version || argv.v) {
+	var packageInfo = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf-8'));
+	console.log(packageInfo.version);
+	process.exit();
+}
+
+// 获取命令及参数
 var [cmd, ...args] = optimist.argv._;
 
 if (['build', 'debug'].indexOf(cmd) < 0) {
